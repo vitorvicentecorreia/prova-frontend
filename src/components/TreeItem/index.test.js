1846cb93fcf4c2257b999b60f6197c0a539e3f7d
Vitor Vicente <vitorvicentecorreia@gmail.com>
2021-01-28 00:49:49 -0300

    Corrigir testes do TreeItem

--- src/components/TreeItem/index.test.js
+++ src/components/TreeItem/index.test.js
@@ -1,7 +1,7 @@
 import { screen, render, fireEvent } from '@testing-library/react'
 
 import TreeItem from '.'
-import { treeWithChildren, treeWithoutChildren, treeWithoutSomeChildrens } from '../../mocks/tree'
+import { treeWithChildren, treeWithoutChildren, treeWithSomeChildrens } from '../../mocks/tree'
 
 import { TreeContextProvider } from '../../context/tree'
 
@@ -21,7 +21,7 @@ describe("TreeItem", () => {
     test(`Dado que o componente foi renderizado
     e recebeu a prop node com nós filhos,
     deve renderizar o checkbox, o name e o ícone de seta`, () => {
-        renderWithContext(<TreeItem node={treeWithChildren} />)
+        renderWithContext(<TreeItem node={Object.values(treeWithChildren)[0]} />)
 
         expect(screen.getByRole('checkbox')).toBeVisible()
         expect(screen.getByText('Richard Paul M.')).toBeVisible()
@@ -39,7 +39,7 @@ describe("TreeItem", () => {
             'Alan G. William'
         ]
 
-        renderWithContext(<TreeItem node={treeWithChildren} />)
+        renderWithContext(<TreeItem node={Object.values(treeWithChildren)[0]} />)
         
         expect(screen.getByText(textsOnScreen[0])).toBeVisible()
         textsOnScreen.shift()
@@ -54,7 +54,7 @@ describe("TreeItem", () => {
     test(`Dado que o componente foi renderizado
     e recebeu a prop node sem nós filhos,
     o icone de seta não deverá ser renderizado.`, async () => {
-        renderWithContext(<TreeItem node={treeWithoutChildren} />)
+        renderWithContext(<TreeItem node={Object.values(treeWithoutChildren)[0]} />)
 
         expect(screen.getByText('Alan G. William')).toBeVisible()
         expect(screen.queryByTitle(/Ícone de seta/i)).not.toBeInTheDocument()
@@ -64,22 +64,23 @@ describe("TreeItem", () => {
     e recebeu a prop node com alguns filhos,
     ao clicar no titulo do nó atual, ele deve ficar com o checkbox com icone de certo,
     e ao expandir esse nó, os filhos devem estar também com o checkbox com ícone de certo`, () => {
-        renderWithContext(<TreeItem node={treeWithoutSomeChildrens} />)
+        renderWithContext(<TreeItem node={Object.values(treeWithSomeChildrens)[0]} />)
 
         fireEvent.click(screen.getByText('Alan G. William'))
         expect(screen.getByTitle(/Ícone de certo/i)).toBeInTheDocument()
 
-        fireEvent.click(screen.getAllByTitle(/Ícone de seta/i)[0])
+        fireEvent.click(screen.getByTitle(/Ícone de seta/i).closest('svg'))
         expect(screen.getAllByTitle(/Ícone de certo/i).length).toBe(5)
+        fireEvent.click(screen.getByTitle(/Ícone de seta/i).closest('svg'))
     })
 
     test(`Dado que o componente foi renderizado
     e receebeu a prop node com alguns filhos,
     ao expandir o nó atual e selecionar alguns, não todos, filhos,
     o nó pai deve ficar com o checkbox com ícone de menos.`, async () => {
-        renderWithContext(<TreeItem node={treeWithoutSomeChildrens} />)
+        renderWithContext(<TreeItem node={Object.entries(treeWithSomeChildrens)[0][1]} />)
 
-        fireEvent.click(screen.getAllByTitle(/Ícone de seta/i)[0])
+        fireEvent.click(screen.getByTitle(/Ícone de seta/i).closest('svg'))
         fireEvent.click(await screen.findByText('Vitor Vicente'))
         fireEvent.click(await screen.findByText('Caroline Rodrigues'))
 
